13121f9d014854b0e16b69d3f14d3c60c0a38da0
MartínGómezPorras <martingomezporras7@gmail.com>
2025-11-23 15:36:47 +0100

    GómezPorras_Martín_M3-UF1

--- GómezPorras_Martín_M3-UF1/GómezPorras_Martín_M3-UF1/GómezPorras_Martín_M3-UF1.cpp
+++ GómezPorras_Martín_M3-UF1/GómezPorras_Martín_M3-UF1/GómezPorras_Martín_M3-UF1.cpp
@@ -1,225 +1,190 @@
 #include <stdio.h>
 #include <math.h>
 
-float suma(float a, float b);
-float resta(float a, float b);
-float multi(float a, float b);
-float division(float a, float b, int* error);
-float potencia(float a, float b);
-int factorial(int n);
-void operacion(int n1, int n2, char oper, float* resultado, int* error);
-char menu(int ejercicio, int* error);
+void operacion(int n1, int n2, char oper, float* resul, int* error);
+int menu();
 
-// Ejercicio 1
-/*
+void Ejercicio1();
+void Ejercicio2();
+void Ejercicio3();
+
 int main() {
-    int n1, n2;
-    char oper;
-    float resultado = 0;
-    int error = 0;
-
-    printf("Introduce dos numeros enteros: \n");
-    scanf_s("%d %d", &n1, &n2);
-
-    printf("MENU\n");
-    printf("+ --> Sumar\n");
-    printf("- --> Restar\n");
-    printf("* --> Multiplicar\n");
-    printf("/ --> Dividir\n");
-    printf("^ --> Potencia\n");
-    printf("s --> Salir\n");
-    printf("Elija opcion: \n");
-    scanf_s(" %c", &oper, 1);
-    if (!(oper == '+' || oper == '-' || oper == '*' || oper == '/' || oper == '^' || oper == 's')) {
-        printf("Error: opcion invalida en el menu\n");
-        error = -1;
-        return 0;
-    }
-    else {
-        error = 1;
-    }
-    operacion(n1, n2, oper, &resultado, &error);
-
-    if (error == 1 && oper != 's') {
-        printf("Resultado: %.2f\n", resultado);
-    }
-    else if (error == -1) {
-        printf("Error: operación no válida.\n");
-    }
-    else if (oper == 's') {
-        printf("Has escogido salir.\n");
-    }
+    Ejercicio1();
+    printf("\n");
+    Ejercicio2();
+    printf("\n");
+    Ejercicio3();
     return 0;
 }
-*/
 
-// Ejercicio 2
-/*
-int main() {
-    int n1, n2;
-    char oper;
-    float resultado = 0;
-    int error = 0;
-    int error_menu = 0;
-
-    printf("Introduce dos numeros enteros: \n");
-    scanf_s("%d %d", &n1, &n2);
-
-    oper = menu(2, &error_menu);
-    if (error_menu == -1) {
-        printf("Error: opcion invalida en el menu\n");
-        return 1;
-    }
-
-    operacion(n1, n2, oper, &resultado, &error);
-
-    if (error == 1 && oper != 's') {
-        printf("Resultado: %.2f\n", resultado);
-    }
-    else if (error == -1) {
-        printf("Error: operacion no valida.\n");
-    }
-    else if (oper == 's') {
-        printf("Has escogido salir.\n");
-    }
-    return 0;
-}
-*/
-
-// Ejercicio 3
-/*
-int main() {
-    int n1, n2;
-    char oper;
-    float resultado = 0;
-    int error = 0;
-    int error_menu = 0;
-
-    printf("Introduce dos numeros enteros: \n");
-    scanf_s("%d %d", &n1, &n2);
-
-    oper = menu(3, &error_menu);
-    if (error_menu == -1) {
-        printf("Error: opcion invalida en el menu\n");
-        return 1;
-    }
-
-    operacion(n1, n2, oper, &resultado, &error);
-
-    if (error == 1 && oper != '!' && oper != 's') {
-        printf("Resultado: %.2f\n", resultado);
-    }
-    else if (error == -1) {
-        printf("Error: operacion no valida.\n");
-    }
-    else if (oper == 's') {
-        printf("Has escogido salir.\n");
-    }
-    return 0;
-}
-*/
-
-char menu(int ejercicio, int* error) {
+int menu() {
     char opcion;
-    printf("MENU\n");
-    printf("+ --> Sumar\n");
-    printf("- --> Restar\n");
-    printf("* --> Multiplicar\n");
-    printf("/ --> Dividir\n");
-    printf("^ --> Potencia\n");
-    if (ejercicio == 3) {
-        printf("! --> Factorial\n");
-    }
-    printf("s --> Salir\n");
-    printf("Elija opcion: \n");
-    scanf_s(" %c", &opcion, 1);
+    int ej;
+    do {
+        printf("Que ejercicio esta haciendo? 2 o 3\n");
+        scanf_s("%d", &ej);
+        if (ej == 2 || ej == 3) {
+            printf("\nMENU\n");
+            printf("+ --> Sumar\n");
+            printf("- --> Restar\n");
+            printf("* --> Multiplicar\n");
+            printf("/ --> Dividir\n");
+            printf("^ --> Potencia\n");
+            if (ej == 3) {
+                printf("! --> Factorial (Solo puede calcular hasta el num 12)\n");
+            }
+            printf("s --> Salir\n");
 
-    if (ejercicio != 3 && opcion == '!') {
-        printf("Respuesta invalida\n");
-        *error = -1;
-    }
-    else {
-        *error = 0;
-    }
+            printf("Elija opcion: ");
+            scanf_s(" %c", &opcion, 1);
 
-    return opcion;
+            return opcion;
+        }
+        else {
+            printf("Ejercicio no existente\n");
+        }
+    }
+    while (!(ej == 2 || ej == 3));
 }
 
-void operacion(int n1, int n2, char oper, float* resultado, int* error) {
-    int f1, f2;
+void operacion(int n1, int n2, char oper, float* resul, int* error) {
+    *error = 1;
+
     switch (oper) {
-    case '+':
-        *resultado = suma(n1, n2);
-        *error = 1;
+    case '+': 
+        *resul = n1 + n2; 
         break;
-    case '-':
-        *resultado = resta(n1, n2);
-        *error = 1;
+    case '-': 
+        *resul = n1 - n2; 
         break;
-    case '*':
-        *resultado = multi(n1, n2);
-        *error = 1;
+    case '*': 
+        *resul = n1 * n2; 
         break;
     case '/':
-        if (n2 == 0)
+        if (n2 == 0) {
             *error = -1;
+        }
         else {
-            *resultado = division(n1, n2, error);
-            *error = 1;
+            *resul = (float)n1 / n2;
         }
         break;
     case '^':
-        *resultado = potencia(n1, n2);
-        *error = 1;
+        *resul = pow(n1, n2);
         break;
+
     case '!':
-        if (n1 < 0 || n2 < 0) {
-            *error = -1;
-        }
-        else {
-            f1 = factorial(n1);
-            f2 = factorial(n2);
-            printf("Factorial de %d = %d\n", n1, f1);
-            printf("Factorial de %d = %d\n", n2, f2);
-            *error = 1;
-        }
+        // se calcula en ej3
         break;
+
     case 's':
-        *error = 1;
+        printf("Has escogido salir\n");
         break;
+
     default:
         *error = -1;
-        break;
     }
 }
 
-float suma(float a, float b) {
-    return a + b;
-}
+void Ejercicio1() {
+    int n1, n2;
+    float resultado;
+    int error;
+    char oper;
 
-float resta(float a, float b) {
-    return a - b;
-}
+    printf("EJ1 - Introduce dos numeros enteros:\n");
+    scanf_s("%d %d", &n1, &n2);
 
-float multi(float a, float b) {
-    return a * b;
-}
-
-float division(float a, float b, int* error) {
-    return a / b;
-}
-
-float potencia(float a, float b) {
-    return pow(a, b);
-}
-
-int factorial(int n) {
-    if (n == 0) {
-        return 1;
+    printf("\nMENU\n");
+    printf("+ --> Sumar\n");
+    printf("- --> Restar\n");
+    printf("* --> Multiplicar\n");
+    printf("/ --> Dividir\n");
+    printf("^ --> Potencia\n");
+    printf("s --> Salir\n");
+    printf("Elija opcion: ");
+    scanf_s(" %c", &oper, 1);
+    
+    if (oper == '!') {
+        printf("Factorial no disponible en este ejercicio\n");
+        return;
     }
-    int r = 1;
-    for (int i = 1; i <= n; i++) {
-        r *= i;
+
+    operacion(n1, n2, oper, &resultado, &error);
+
+    if (error == 1 && oper != 's') {
+        printf("Resultado: %.2f\n", resultado);
     }
-    return r;
+    else if (error == -1) {
+        printf("Error en la operacion.\n");
+    }
+}
+
+void Ejercicio2() {
+    int n1, n2;
+    float resultado;
+    int error;
+    char oper;
+
+    printf("EJ2 - Introduce dos numeros enteros:\n");
+    scanf_s("%d %d", &n1, &n2);
+
+    oper = menu();
+
+    if (oper == '!') {
+        printf("Factorial no disponible en este ejercicio\n");
+        return;
+    }
+
+    operacion(n1, n2, oper, &resultado, &error);
+
+    if (error == 1 && oper != 's') {
+        printf("Resultado: %.2f\n", resultado);
+    }
+    else if (error == -1) {
+        printf("Error en la operacion.\n");
+    }
+}
+
+void Ejercicio3() {
+    int n1, n2;
+    float resultado;
+    int error;
+    char oper;
+
+    printf("EJ3 - Introduce dos numeros enteros:\n");
+    scanf_s("%d %d", &n1, &n2);
+
+    oper = menu();
+
+    if (oper == '!') {
+        //n1
+        if (n1 >= 0 && n1 <= 12) {
+            int fact1 = 1;
+            for (int i = 1; i <= n1; i++) {
+                fact1 *= i;
+            }
+            printf("El factorial de %d es %d\n", n1, fact1);
+        }
+        else printf("Numero 1, no esta entre el 0 y el 12\n");
+
+        //n2
+        if (n2 >= 0 && n2 <= 12) {
+            int fact2 = 1;
+            for (int i = 1; i <= n2; i++) {
+                fact2 *= i;
+            }
+            printf("El factorial de %d es %d\n", n2, fact2);
+        }
+        else printf("Numero 2, no esta entre el 0 y el 12\n");
+        return;
+    }
+
+    operacion(n1, n2, oper, &resultado, &error);
+
+    if (error == 1 && oper != 's') {
+        printf("Resultado: %.2f\n", resultado);
+    }
+    else if (error == -1) {
+        printf("Error en la operacion.\n");
+    }
 }
